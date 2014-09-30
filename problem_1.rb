b848f126ef108a5153929bdf85afff901fcad007
Kamilah Jenkins <kamilahjae@gmail.com>
2014-09-30 12:34:37 -0700

    Update problem_1.rb

--- problem_1.rb
+++ problem_1.rb
@@ -1,11 +1,13 @@
 # sum all multiples of 3 or 5 below 1000
 
-numbers = 0...1000
+@numbers = 0...1000
 
-sum = 0
-numbers.each do |number|
-  if number % 3 == 0 || number % 5 == 0
-    sum += number
+def sum
+  sum = 0
+  @numbers.each do |number|
+    if number % 3 == 0 || number % 5 == 0
+      sum += number
+    end
   end
   sum
 end
